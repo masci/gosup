9d96d96fb049818f38f3e066ccf7572c1286f8ce
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2010-10-22 21:12:43 -0500

    ok defined a supervisor loop
    Still need to define the stop procedure.

--- src/supervisor.go
+++ src/supervisor.go
@@ -18,6 +18,7 @@ type Policy int
 const (
 	ALWAYS = iota // 0 value and therefore the default for restart policies
 	NEVER
+	DIEALSO
 )
 
 type ServiceSpec struct {
@@ -44,12 +45,59 @@ func (sup *Supervisor) UnregisterService(name string) bool {
 	return true
 }
 
-func (sup *Supervisor) Start() bool {
-	return sup.doForServices(func(s *ServiceSpec) bool {
-	        ch, result := s.service.Start()
-		s.ping = ch
-		return result
-	}) 
+func serviceStarter(s *ServiceSpec) bool {
+	ch, result := s.service.Start()
+	s.ping = ch
+	return result
+}
+
+func (sup *Supervisor) Start() (chan bool, bool) { // A supervisor is a service
+	result := sup.doForServices(serviceStarter)
+
+	if !result {
+		return nil, false
+	}
+
+	ping := make(chan bool)	
+
+	//run supervisor loop
+	go sup.Loop(ping)
+	return ping, true
+}
+
+func (sup *Supervisor) Loop(ch chan bool) {
+	for true {
+		result := sup.doForServices(func(s *ServiceSpec) bool {
+			// check for service aliveness
+			restart := false
+			ch := s.ping
+			if ch == nil || closed(ch) {
+				restart = true
+			} else {
+				ch<- true // send a ping
+				ok := <-ch // listen for response
+				if !ok {
+					restart = true
+				}
+			}
+			// if restart is needed follow restart policy
+			if restart {
+				switch s.restartPolicy {
+					case ALWAYS:
+						serviceStarter(s)
+					case DIEALSO:
+					 	return false
+				}
+			}
+			// return true if supervisor tree is still valid
+			return true
+		})
+		if !result {
+			sup.Stop()
+			break
+		}
+	}
+	close(ch)
 }
 
 func (sup *Supervisor) Stop() bool {
