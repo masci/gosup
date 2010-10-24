15e715758f3cd9d5a669267d07afabbaa7ecee58
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2010-10-23 19:24:10 -0500

    Copyright and License information

--- src/supervisor.go
+++ src/supervisor.go
@@ -1,4 +1,8 @@
 /*
+ Copyright 2010 Jeremy Wall (jeremy@marzhillstudios.com)
+ Use of this source code is governed by the Artistic License 2.0.
+ That License is included in the LICENSE file.
+ 
  The supervisor package implements an elementary supervision tree
  for goroutines ala erlangs supervision trees.
 
